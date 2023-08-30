13d6920f714df68344f545f69a5a68d4bf923261
FelipeMDantas <felipemdantas@terra.com.br>
2023-08-29 23:56:41 -0300

    Hero section further stylized.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,13 +15,18 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl">
-          <span>The New Arrival</span>
+        <h1
+          className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading[82] 
+        font-bold"
+        >
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span>
+          <span className="text-coral-red inline-block mt-3">Nike</span>
           Shoes
         </h1>
-        <p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
