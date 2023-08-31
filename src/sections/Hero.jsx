d2e107b3454b071ec11912ae0dc276b093fea036
FelipeMDantas <felipemdantas@terra.com.br>
2023-08-30 23:53:04 -0300

    ShoeCard component created.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import { arrowRight } from "../assets/icons";
+import { bigShoe1 } from "../assets/images";
 import Button from "../components/Button";
-import { statistics } from "../constants";
+import ShoeCard from "../components/ShoeCard";
+import { shoes, statistics } from "../constants";
 
 const Hero = () => {
   return (
@@ -23,8 +25,7 @@ const Hero = () => {
             The New Arrival
           </span>
           <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span>
-          Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
@@ -35,8 +36,30 @@ const Hero = () => {
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl font-palaquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrate text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div
+        className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40
+      bg-primary bg-hero bg-cover bg-center"
+      >
+        <img
+          src={bigShoe1}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+
+        <div>
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard imrURL={shoe} />
             </div>
           ))}
         </div>
